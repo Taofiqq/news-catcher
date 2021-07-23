106626b433826645293cfcb4f090581563676675
User <taofiqaiyelabegan45@gmail.com>
2021-07-23 10:31:25 +0800

    styled the card

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const newsRouter = require('./src/routes/news');
 app.use('/', newsRouter);
-app.use('/article', newsRouter);
+// app.use('/article', newsRouter);
 
 
 
